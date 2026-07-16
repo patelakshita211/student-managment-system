69f15c713367ff6fb9c29c4cc439269735511a4e
Akshita Patel <patelakshita211@gamil.com>
2026-07-16 22:10:17 +0530

    Added Even Odd Program using Arrow Function

---
+++ odd-even.js
@@ -0,0 +1,10 @@
+const checkEvenOdd = (number) => {
+    if (number % 2 === 0) {
+        console.log(`${number} is an even number;`);
+    }else {
+        console.log(`${number} is an odd number;`);
+    }
+}
+
+checkEvenOdd(5);
+checkEvenOdd(10);
