fe135e6e5839f1c1cc8c6bcde378e24407208339
Akshita Patel <patelakshita211@gamil.com>
2026-07-16 22:48:49 +0530

    swap program using destructing

---
+++ swap-variables.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+
+console.log("Before swapping:");
+console.log("a", a);
+console.log("b", b);
+
+//swap array descructing
+[a,b] = [b,a];
+
+console.log("After Swapping");
+console.log("a", a);
+console.log("b", b);
